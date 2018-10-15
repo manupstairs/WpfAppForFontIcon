ea5597cf57ea852b9c0900c3db4d8a825c2bc126
Zheng_Xing <zheng_xing@dell.com>
2018-10-15 17:40:21 +0800

    add demo projects

---
+++ AppWithFontIcon/FontIcons.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWithFontIcon
+{
+    public static class FontIcons
+    {
+        public static string airplane_mode_circ { get; } = "\ue900";
+        public static string bluetooth_inactive { get; } = "\ue901";
+        public static string brightness { get; } = "\ue902";
+        public static string brightness_inactive { get; } = "\ue903";
+        public static string browse_inactive { get; } = "\ue904";
+        public static string camera { get; } = "\ue905";
+        public static string camera_flash_auto_circ { get; } = "\ue906";
+        public static string camera_flash_off_circ { get; } = "\ue907";
+        public static string camera_flash_on_circ { get; } = "\ue908";
+        public static string camera_swap_inactive { get; } = "\ue909";
+        public static string camera_circ { get; } = "\ue90a";
+        public static string cellular_broadband_inactive { get; } = "\ue90b";
+        public static string display_inactive { get; } = "\ue90c";
+        public static string flashlight_inactive { get; } = "\ue90d";
+        public static string fn_lock_inactive { get; } = "\ue90e";
+        public static string gps_inactive { get; } = "\ue90f";
+        public static string keyboard_backlight_inactive { get; } = "\ue910";
+        public static string microphone_inactive { get; } = "\ue911";
+        public static string microphone_mute_inactive { get; } = "\ue912";
+        public static string night_mode_on_inactive { get; } = "\ue913";
+        public static string num_lock_inactive { get; } = "\ue914";
+        public static string on_screen_keyboard_inactive { get; } = "\ue915";
+        public static string project_screen { get; } = "\ue916";
+        public static string rotation_lock_inactive { get; } = "\ue917";
+        public static string rugged_close { get; } = "\ue918";
+        public static string rugged_open { get; } = "\ue919";
+        public static string screen_projector { get; } = "\ue91a";
+        public static string scroll_lock_inactive { get; } = "\ue91b";
+        public static string touchscreen_on_circ { get; } = "\ue91c";
+        public static string volume_circ { get; } = "\ue91d";
+        public static string volume_high { get; } = "\ue91e";
+        public static string volume_low { get; } = "\ue91f";
+        public static string wi_gig_circ { get; } = "\ue920";
+        public static string wireless_circ { get; } = "\ue921";
+        public static string wwan_circ { get; } = "\ue922";
+        public static string als_on { get; } = "\ue923";
+        public static string open_mobilty_center { get; } = "\ue924";
+        public static string open_rcc_square { get; } = "\ue925";
+        public static string refresh_page { get; } = "\ue926";
+        public static string rotate_display { get; } = "\ue927";
+        public static string stealth_mode_on { get; } = "\ue928";
+        public static string url { get; } = "\ue929";
+        public static string windows_setting_common { get; } = "\ue92a";
+        public static string barcode { get; } = "\ue92b";
+        public static string barcode_scanner_circ { get; } = "\ue92c";
+
+
+    }
+}
---
+++ IcoMoonReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace IcoMoonReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().ReadSvgFile();
+        }
+
+        private void ReadSvgFile()
+        {
+            using (var stream = new FileStream("rcc-fonticon-ribbon-v2.svg", FileMode.Open))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var pattern = "unicode(\\S)*\\sglyph-name(\\S)*\"";
+                    var input = reader.ReadToEnd();
+                    foreach (Match match in Regex.Matches(input, pattern))
+                    {
+                        pattern = "\"\\S*\"";
+                        var list = new List<string>();
+                        foreach (var result in Regex.Matches(match.Value, pattern))
+                        {
+                            list.Insert(0, result.ToString());
+                        }
+                        var name = list[0].Replace("\"", "").Replace("-","_");
+                        var code = list[1].Replace("&#x", "\\u").Replace(";", "");
+                        Console.WriteLine($"public static string {name} {{ get; }} = {code};");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
